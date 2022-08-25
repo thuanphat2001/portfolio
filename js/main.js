65aca9461246fa0171fe8bd5c469f71df436006b
thuanphat2001 <95758970+thuanphat2001@users.noreply.github.com>
2022-08-25 20:27:03 +0700

    responsive done

--- js/main.js
+++ js/main.js
@@ -22,4 +22,19 @@ $(document).ready(function () {
       544: { items: 2 },
     },
   });
+  // sticky navigation menu
+  let nav_offset_top = $(".header").height() + 50;
+  function navbarFixed() {
+    if ($(".header").length) {
+      $(window).scroll(function () {
+        let scroll = $(window).scrollTop();
+        if (scroll >= nav_offset_top) {
+          $(".header .main-menu").addClass("navbar_fixed");
+        } else {
+          $(".header .main-menu").removeClass("navbar_fixed");
+        }
+      });
+    }
+  }
+  navbarFixed();
 });
